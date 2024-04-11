5b7bb88efe63410badcd2d4c44f048a6fa403034
Eric <gaoyuxuan.cn@gmail.com>
2024-04-11 22:06:25 +0800

    lab01 finished.

--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -3,4 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node* fast_ptr = head;
+    node* slow_ptr = head;
+    int turn = 0;
+    while (fast_ptr != NULL) {
+        fast_ptr = fast_ptr->next;
+        if (turn % 2 == 1) {
+            slow_ptr = slow_ptr -> next;
+        }
+        if (fast_ptr == slow_ptr) {
+            return 1;
+        }
+        ++turn;
+    }
+    return 0;
 }
